65abfd7e52e26b8fc11ac09c514ce19486890416
Sarah Jahan <sarah-jahan@live.com>
2024-11-20 18:50:41 -0500

    create table migrations"

---
+++ migrations/20241120225628_create_games_tables.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema
+      .createTable("games", (table) => {
+        table.increments("id").primary();
+        table.integer('user_id').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE');
+        table.string("title").notNullable();
+        table.string('summary').notNullable();
+        table.string('coverArt').notNullable();
+        table.string('status').defaultTo('Not Played');
+        table.integer('rating').notNullable().defaultTo(0);
+        table.text('notes');
+        table.string('tags');
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table
+          .timestamp("updated_at")
+          .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+      });
+  }
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  export function down(knex) {
+    return knex.schema.dropTable("games");
+  }
---
+++ migrations/20241120233236_create_tags_tables.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema
+      .createTable("tags", (table) => {
+        table.increments("id").primary();
+        table.string("name").notNullable().unique();
+        table.integer('user_id').unsigned().notNullable().references('users.id').onUpdate('CASCADE').onDelete('CASCADE');
+        table.integer('game_id').unsigned().notNullable().references('games.id').onUpdate('CASCADE').onDelete('CASCADE');
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table
+          .timestamp("updated_at")
+          .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+      })
+  }
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  export function down(knex) {
+    return knex.schema.dropTable("tags");
+  }
