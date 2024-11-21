25170c01754e71a93d986c5a843cd8f93a353c86
Sarah Jahan <sarah-jahan@live.com>
2024-11-20 22:37:07 -0500

    setup and test api server request

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // import usersRoutes from "./routes/users-routes.js";
-// import gamesRoutes from "./routes/games-routes.js";
+import gamesRoutes from "./routes/games-routes.js";
 // import tagsRoutes from "./routes/tags-routes.js";
 
 // app.use("/api/users", usersRoutes);
-// app.use("/api/games", gamesRoutes);
+app.use("/api/games", gamesRoutes);
 // app.use("/api/tags", tagsRoutes);
 
 
---
+++ routes/games-routes.js
@@ -0,0 +1,46 @@
+import express from "express";
+import axios from "axios";
+
+const router = express.Router();
+const { API_KEY, CLIENT_ID } = process.env
+
+
+router.route("/")
+.get((req, res) => {
+    const getGamesList = async (req, res) => {
+        try {
+            const query = `
+                fields name, genres.name, cover.url;
+                where aggregated_rating > 95;
+                sort aggregated_rating asc;
+                limit 20;`;
+          const data = await axios.post("https://api.igdb.com/v4/games", query, {
+            headers: {
+                'Client-ID': CLIENT_ID,
+                'Authorization': API_KEY
+            }
+        })
+          console.log(data)
+        //   res.status(200).json(data);
+
+        } catch (err) {
+            console.log(err)
+            // res.status(400).send(`Error retrieving games: ${err}`);
+
+        }};
+    
+    getGamesList()
+});
+
+
+
+
+
+    
+
+
+
+
+
+
+export default router;
