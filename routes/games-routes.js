c626e0b67938b146ccc9d6bc5084dd3933ca3f98
Sarah Jahan <sarah-jahan@live.com>
2024-11-21 14:50:38 -0500

    clean up and fix front-end server connection

--- routes/games-routes.js
+++ routes/games-routes.js
@@ -4,32 +4,32 @@ import axios from "axios";
 const router = express.Router();
 const { API_KEY, CLIENT_ID } = process.env
 
+const getGamesList = async () => {
+    try {const query = `
+            fields name, genres.name, cover.url;
+            where aggregated_rating > 95;
+            sort aggregated_rating asc;
+            limit 20;`;
+            const data = await axios.post("https://api.igdb.com/v4/games", query, {
+                headers: {
+                    'Client-ID': CLIENT_ID,
+                    'Authorization': API_KEY
+                }
+            })
+            console.log("Games retrieved from API");
+            return data;
+    } catch (err) {
+        console.error("Error retreiving games:", error)
+    }};
 
 router.route("/")
-.get((req, res) => {
-    const getGamesList = async (req, res) => {
-        try {
-            const query = `
-                fields name, genres.name, cover.url;
-                where aggregated_rating > 95;
-                sort aggregated_rating asc;
-                limit 20;`;
-          const data = await axios.post("https://api.igdb.com/v4/games", query, {
-            headers: {
-                'Client-ID': CLIENT_ID,
-                'Authorization': API_KEY
-            }
-        })
-          console.log("Games retrieved from API")
-        //   res.status(200).json(data);
-
-        } catch (err) {
-            console.log(err)
-            // res.status(400).send(`Error retrieving games: ${err}`);
-
-        }};
-    
-    getGamesList()
+.get(async (req, res) => {
+    try {
+        const games = await getGamesList(); 
+        res.status(200).json(games.data);
+    }catch (err) {
+        res.status(500).send(`Error retreiving games: ${err.message}`)
+    }
 });
 
 
