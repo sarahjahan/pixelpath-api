e269fcf6e7c0e23adff2a2b9932aafa4cade41fd
Sarah Jahan <sarah-jahan@live.com>
2024-12-11 21:30:20 -0500

    minor db name update

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ export default {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   },
