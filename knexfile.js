4f998f8649995e27eefd3f98bdbdd9a4e861a35d
Sarah Jahan <sarah-jahan@live.com>
2024-12-11 23:49:00 -0500

    match knexfile to env

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,9 @@ import "dotenv/config";
 export default {
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.MYSQLHOST,
+    database: process.env.MYSQLDATABASE,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
   },
 };
