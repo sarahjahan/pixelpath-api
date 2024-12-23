f1f78c4b5f84eecf966ab37e96ce71913c7eb4f8
Sarah Jahan <sarah-jahan@live.com>
2024-12-23 16:18:42 -0500

    update users table migration and install package dependencies

--- migrations/20241120225619_create_users_tables.js
+++ migrations/20241120225619_create_users_tables.js
@@ -9,6 +9,7 @@ export function up(knex) {
     table.string("email").notNullable();
     table.string("password").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
+
     table
       .timestamp("updated_at")
       .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
